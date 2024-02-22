d9f0985fc3be287435123fcb1fb9dd8c6dd8e079
teli203 <teli203@aol.com>
2024-02-21 22:25:10 -0600

    updating JS with const and all div's

---
+++ script.js
@@ -0,0 +1,11 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatheritemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+setInterval(() => {
+
+}, 1000);
