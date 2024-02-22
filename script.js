bfc4a7c558a46cc50e94a563ece641b8ddace612
teli203 <teli203@aol.com>
2024-02-21 22:37:40 -0600

    setInterval for time,mth,day,date,hr,min,ampm

--- script.js
+++ script.js
@@ -7,5 +7,15 @@ const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
 setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
 
 }, 1000);
